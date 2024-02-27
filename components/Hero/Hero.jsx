a60cf3a653656402a817188d5ea2807403139bee
m.k <m.kotou0715@gmail.com>
2024-02-28 07:43:58 +0900

    heroへmotion追加

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -9,6 +9,13 @@ import "swiper/css/bundle";
 import { BsGithub } from "react-icons/bs";
 import Link from "next/link"
 import styles from "./hero.module.scss";
+import { motion } from 'framer-motion';
+
+//motion
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
 
 export default function Hero() {
 
@@ -42,6 +49,11 @@ export default function Hero() {
       >
           <SwiperSlide className={styles.slideItem}>
             <span className={styles.swiperSubTitle}>MISSION</span>
+            <motion.div
+              variants={containerVariants}
+              initial="hidden"
+              animate="visible"
+            >
             <h2 className={styles.swiperTitle}>心動かすデザインと保守性・再利用性の高いコードを</h2>
             <div className={styles.text}>
               <p>
@@ -74,14 +86,15 @@ export default function Hero() {
                 にてAPIで過去制作サイトを配信しています。
               </p>
             </div>
-          <Link
-            className={styles.link}
-            href="https://github.com/mai-007/MEE_portfolio"
-            rel="noopener noreferrer" target="_blank"
+            <Link
+              className={styles.link}
+              href="https://github.com/mai-007/MEE_portfolio"
+              rel="noopener noreferrer" target="_blank"
             >
             <BsGithub />
             &nbsp;ソースコードはこちら
-          </Link>
+            </Link>
+            </motion.div>
         </SwiperSlide>
       </Swiper>
   );
